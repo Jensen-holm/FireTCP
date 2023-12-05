23b811956215ce1034f7cd7da50de262d8e571c2
Jensen-holm <holmj@mail.gvsu.edu>
2023-12-05 13:20:38 -0500

    working on testing directory, will need to install FireApi with

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@ git clone https://github.com/Jensen-holm/FireApi.git
 
 cd FireApi
 
-rm -rf .git .gitignore
+rm -rf .git .gitignore test
+
+rm -f install.sh
 
 cd FireApi && mv * ..
 
 cd .. && rm -rf FireApi examples
-
-rm install.sh
-

4efcb086bf4ad4391c8385b6c082ad1064a45e58
Jensen-holm <holmj@mail.gvsu.edu>
2023-12-06 14:24:15 -0500

    made the install script way cooler

--- install.sh
+++ install.sh
@@ -1,6 +1,35 @@
 #! /bin/bash
 
-git clone https://github.com/Jensen-holm/FireApi.git
+FIRE="🔥"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+
+
+suppress_std_out() {
+    exec 3>&1
+    exec > /dev/null
+}
+
+restore_std_out() {
+    exec 1>&3
+}
+
+print_done() {
+    echo -e "✅\n"
+}
+
+echo -e "${FIRE}${FIRE}${CYAN}Installing FireApi${RESET}${FIRE}${FIRE}\n"
+
+echo -e -n "${BLUE}cloning the repo${RESET} (https://github.com/Jensen-holm/FireApi) ... "
+
+git clone https://github.com/Jensen-holm/FireApi.git > /dev/null 2>&1
+
+print_done
+
+suppress_std_out
 
 cd FireApi
 
@@ -16,6 +45,20 @@ cd ..
 
 pwd
 
+restore_std_out
+
+echo -e -n "${BLUE}packaging FireApi/ module into FireApi.📦${RESET} ... "
+
+suppress_std_out
+
 mojo package FireApi -o FireApi.📦
 
+restore_std_out
+print_done
+suppress_std_out
+
 rm -rf FireApi
+
+restore_std_out
+
+echo -e "${FIRE}${FIRE}${GREEN} FireApi Installed Successfull ${RESET}${FIRE}${FIRE}\n"
